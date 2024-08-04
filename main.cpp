055b08472d0aa099701349a5fb23d2fe892c2b20
vlifirenko <vladislav.lifirenko@gmail.com>
2024-08-04 18:21:07 +0300

    Shading. Moving Forward.

--- main.cpp
+++ main.cpp
@@ -32,15 +32,15 @@ int main()
 	map += L"#..............#";
 	map += L"#..............#";
 	map += L"#..............#";
+	map += L"#..........#...#";
+	map += L"#..........#...#";
 	map += L"#..............#";
 	map += L"#..............#";
 	map += L"#..............#";
 	map += L"#..............#";
 	map += L"#..............#";
 	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
+	map += L"#.......########";
 	map += L"#..............#";
 	map += L"#..............#";
 	map += L"################";
@@ -61,10 +61,22 @@ int main()
 		// controls
 		// handle CCW rotation
 		if (GetAsyncKeyState((unsigned short)'A') & 0x8000)
-			fPlayerA -= (0.5f) * fElapsedTime;
+			fPlayerA -= (0.8f) * fElapsedTime;
 
 		if (GetAsyncKeyState((unsigned short)'D') & 0x8000)
-			fPlayerA += (0.5f) * fElapsedTime;
+			fPlayerA += (0.8f) * fElapsedTime;
+
+		if (GetAsyncKeyState((unsigned short)'W') & 0x8000)
+		{
+			fPlayerX += sinf(fPlayerA) * 5.0f * fElapsedTime;
+			fPlayerY += cosf(fPlayerA) * 5.0f * fElapsedTime;
+		}
+
+		if (GetAsyncKeyState((unsigned short)'S') & 0x8000)
+		{
+			fPlayerX -= sinf(fPlayerA) * 5.0f * fElapsedTime;
+			fPlayerY -= cosf(fPlayerA) * 5.0f * fElapsedTime;
+		}
 
 		for (int x = 0; x < nScreenWidth; x++)
 		{
@@ -104,12 +116,26 @@ int main()
 			int nCeiling = (float)(nScreenHeight / 2.0) - nScreenHeight / ((float)fDistanceToWall);
 			int nFloor = nScreenHeight - nCeiling;
 
+			// shading
+			short nShade = ' ';
+
+			if (fDistanceToWall <= fDepth / 4.0f)
+				nShade = 0x2588;		// very close
+			else if (fDistanceToWall < fDepth / 3.0f)
+				nShade = 0x2593;
+			else if (fDistanceToWall < fDepth / 2.0f)
+				nShade = 0x2592;
+			else if (fDistanceToWall < fDepth)
+				nShade = 0x2591;
+			else
+				nShade = ' ';		// too far away
+
 			for (int y = 0; y < nScreenHeight; y++)
 			{
 				if (y < nCeiling)
 					screen[y * nScreenWidth + x] = ' ';
 				else if (y > nCeiling && y <= nFloor)
-					screen[y * nScreenWidth + x] = '#';
+					screen[y * nScreenWidth + x] = nShade;
 				else
 					screen[y * nScreenWidth + x] = ' ';
 			}
